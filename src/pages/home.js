dff4ffe7a0d571a0e9988e1b912ee5a256f68524
RobLR <94964242+RobLR@users.noreply.github.com>
2022-07-21 14:30:36 +0100

    - started adding redux to handle global state

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Homepage() {
 	return (
 		<main>
-			<img src={require('./resized-DNDNotes2.webp')} alt="d&d notes" />
+			{/* <img src={require('./resized-DNDNotes2.webp')} alt="d&d notes" /> */}
 			<h2>Welcome!</h2>
 			<p>
 				This is a simple program aimed at keeping track of D&D sessions. Start
